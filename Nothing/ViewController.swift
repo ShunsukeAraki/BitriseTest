53149d1913d5ce312ea099740ccf1037f2c01652
ShunsukeAraki <araki@yadokari.tv>
2019-09-12 20:33:36 +0900

    Initial Commit

---
+++ Nothing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nothing
+//
+//  Created by ShunsukeAraki on 2019/09/12.
+//  Copyright © 2019 ShunsukeAraki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
